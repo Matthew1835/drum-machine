22b56c6190075959260fbd162ac2bf20ebb90c73
Matt <myatthuta1835@gmail.com>
2026-01-05 12:12:58 +0630

    Add complete script (version 1)

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const display = document.getElementById("display");
-const padBank = document.getElementById("pad-bank");
-const padContainer = document.getElementById("pad-container");
 const drumPads = document.querySelectorAll(".drum-pad");
 const audioEl = document.querySelectorAll(".clip");
 
-const audio = new Audio();
+const audioMap = {};
+audioEl.forEach(a => audioMap[a.id] = a);
 
-const playClip = () => {
+const playClip = (audioId) => {
+  const audio = audioMap[audioId]
 
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  let text = audio.parentElement.id;
+  display.innerText = text;
 }
 
 drumPads.forEach(pad => {
-    pad.addEventListener("click", () => console.log(pad.innerText))
+    pad.addEventListener("click", () => playClip(pad.querySelector("audio").id))
 })
 
 document.addEventListener("keydown", e =>
-  console.log(e.key)
+  playClip(e.key.toUpperCase())
 );
